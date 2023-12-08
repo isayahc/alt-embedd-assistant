c01af48cd6282ee7ebd07c61cf44e48db6c35661
isayhc <isayahculbertson@gmail.com>
2023-12-08 02:08:26 -0500

    added a button for updating app states

--- embedd_choices_options.py
+++ embedd_choices_options.py
@@ -9,15 +9,20 @@ def main():
     index_types = ["FLAT", "IVF_FLAT", "IVF_SQ8", "IVF_PQ", "HNSW", "IVF_HNSW", "RHNSW_FLAT", "RHNSW_SQ", "RHNSW_PQ", "ANNOY"]
     nprobe_values: List[int] = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]
 
-    # Create UI elements
-    selected_distance_metric = st.selectbox("Select Distance Metric", distance_metrics)
-    selected_index_type = st.selectbox("Select Index Type", index_types)
-    selected_nprobe_value = st.selectbox("Select Nprobe Value", nprobe_values)
+    # Form for selections
+    with st.form(key='options_form'):
+        selected_distance_metric = st.selectbox("Select Distance Metric", distance_metrics)
+        selected_index_type = st.selectbox("Select Index Type", index_types)
+        selected_nprobe_value = st.selectbox("Select Nprobe Value", nprobe_values)
+
+        # Submit button
+        submit_button = st.form_submit_button(label='Update Choices')
 
     # Displaying the selected options
-    st.write(f"Selected Distance Metric: {selected_distance_metric}")
-    st.write(f"Selected Index Type: {selected_index_type}")
-    st.write(f"Selected Nprobe Value: {selected_nprobe_value}")
+    if submit_button:
+        st.write(f"Selected Distance Metric: {selected_distance_metric}")
+        st.write(f"Selected Index Type: {selected_index_type}")
+        st.write(f"Selected Nprobe Value: {selected_nprobe_value}")
 
 if __name__ == "__main__":
     main()
